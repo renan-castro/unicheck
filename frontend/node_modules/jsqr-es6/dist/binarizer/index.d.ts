6e56994a0d5034cb2073edc4abffcda6378b2305
Renan de Castro Rodrigues <89025568+renan-castro@users.noreply.github.com>
2025-11-11 19:19:56 -0300

    Delete frontend/node_modules directory

--- frontend/node_modules/jsqr-es6/dist/binarizer/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { BitMatrix } from "../BitMatrix";
-import { GreyscaleWeights } from "../index";
-export declare function binarize(data: Uint8ClampedArray, width: number, height: number, returnInverted: boolean, greyscaleWeights: GreyscaleWeights, canOverwriteImage: boolean): {
-    binarized: BitMatrix;
-    inverted: BitMatrix;
-} | {
-    binarized: BitMatrix;
-    inverted?: undefined;
-};
