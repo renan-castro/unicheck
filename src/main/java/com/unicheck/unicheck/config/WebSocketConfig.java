76df8c7802cc0c20ccaaf0b406a4aa7691d4f915
Renan de Castro Rodrigues <89025568+renan-castro@users.noreply.github.com>
2025-11-11 15:11:03 -0300

    Update WebSocketConfig to allow origin patterns

--- src/main/java/com/unicheck/unicheck/config/WebSocketConfig.java
+++ src/main/java/com/unicheck/unicheck/config/WebSocketConfig.java
@@ -18,7 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/unicheck-websocket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/unicheck-websocket").setAllowedOriginPatterns("*").withSockJS();
     }
 }
+
 
